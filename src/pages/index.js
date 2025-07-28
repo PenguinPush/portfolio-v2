7dc46f88980ed6e7ab2632a4bf4a9da74e56e7fc
PenguinPush <icyzminecraft@gmail.com>
2025-07-28 17:56:12 -0400

    implemented a basic spinner
    i don't know how much i like it though, might change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,18 @@
+import Spinner from '../components/spinner';
 import { DM_Sans } from 'next/font/google';
 
 const dmSans = DM_Sans();
 
-export default function Home() {
+export default function Main() {
   return (
     <div
-      className={`${dmSans.className} flex items-start justify-center text-lg text-black h-screen bg-yellow-base`}>
+      className={`${dmSans.className} flex items-start justify-center text-lg text-white h-screen bg-yellow-base`}>
       <div className="flex flex-col p-4 md:p-12 h-screen gap-2 w-full md:max-w-[700px]">
         <h1 className="text-6xl font-black tracking-tighter text-center text-nowrap">Andrew Dai</h1>
         <div className="flex flex-col items-center px-4">
-          <div className="flex w-full h-8 items-center justify-center overflow-clip bg-yellow-dark">
-            <div className="relative flex gap-8 text-nowrap top-[1px]">
-              <p>8x Hackathon Winner</p>
-              <p>Software Engineer</p>
-              <p>Photographer</p>
-              <p>A</p>
-              <p>Gamer</p>
-            </div>
+          <Spinner/>
           </div>
         </div>
       </div>
-    </div>
   );
 }
